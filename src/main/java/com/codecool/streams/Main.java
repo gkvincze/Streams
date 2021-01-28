f0b50cb4524b529961f642f266dbdc6d6c008aea
Gabor Vincze <gkvincze@gmail.com>
2021-01-28 09:55:25 +0100

    cs

--- src/main/java/com/codecool/streams/Main.java
+++ src/main/java/com/codecool/streams/Main.java
@@ -2,7 +2,25 @@ package com.codecool.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class Main {
-    List<String> myList = Arrays.asList("a1","a2","b1","c2","c1");
+    public static void main(String[] args) {
+        List<String> myList = Arrays.asList("a1","a2","b1","c2","c1");
+        myList
+                .stream()
+                .filter(s->s.startsWith("c"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+        /*
+        List cs = myList.stream().filter(a->a.charAt(0) == 'c').
+                map(word -> word.substring(0, 1).toUpperCase() + word.substring(1)).sorted()
+                .collect(Collectors.toList());
+        cs.forEach(System.out::println);
+
+         */
+    }
+
 }
